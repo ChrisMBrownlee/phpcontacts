a2bbff96fb27eaa0638549ca39ea4b9592557c3e
Chris Brownlee <57460219+ChrisMBrownlee@users.noreply.github.com>
2020-05-07 12:23:58 -0500

    started with zeltavian for new php project

---
+++ includes/data.inc.php
@@ -0,0 +1,42 @@
+<?php
+require 'dbh.inc.php';
+// SQL Query
+$sql = 'SELECT * FROM Contacts';
+$stmt = mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql))
+{
+  header("Location: ../signup.php?error=sqlerror");
+  exit();
+}
+else
+{
+  $source  = array();
+  $final   = array();
+  //excute and store result into $stmt
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+
+
+  while($row = mysqli_fetch_assoc($result)) {
+    array_push($source, $row['contactId']);
+    array_push($source, $row['contactFirst']);
+    array_push($source, $row['contactLast']);
+    array_push($source, $row['contactAddress']);
+    array_push($source, $row['contactNotes']);
+    array_push($final, $source);
+    $source = array();
+  }
+
+  // Print source string
+  $json = json_encode($final);
+  echo $json;
+  $file = fopen('json/contacts.json', "w");
+  fwrite($file, $json);
+  fclose($file);
+
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+ ?>
