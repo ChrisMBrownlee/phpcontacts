2d62bededc6a8b65ddb423010236bb0506a7db47
Chris Brownlee <57460219+ChrisMBrownlee@users.noreply.github.com>
2020-05-08 11:14:07 -0500

    fixed

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
 				<?php
 					if (isset($_SESSION['userId'])){
 						echo '<p class="login-status">You are logged in!</p>';
-						header("Location: http://www.gtrcoders.com/zcbrownlee/contacts.php", true, 301);
-						exit();
-						//require "contacts.php";
+						require "contacts.php";
 					}else{
 						echo '<p class="login-status">You are logged out!</p>';
 					}
